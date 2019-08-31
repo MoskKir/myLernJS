5c28ce8cfc8f55275c47089ea44bde7d25df37dc
MoskKir <47816850+MoskKir@users.noreply.github.com>
2019-09-01 02:54:01 +0300

    add build React app

---
+++ loanBuild/precache-manifest.b44faafb8370e410afde1e17f4e518e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d3e2219962b226365924cd28615d11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbeb616e3d16bf7d9c62",
+    "url": "/static/css/main.0b31ce54.chunk.css"
+  },
+  {
+    "revision": "32376e5f140c0118efdc",
+    "url": "/static/js/2.10818648.chunk.js"
+  },
+  {
+    "revision": "cbeb616e3d16bf7d9c62",
+    "url": "/static/js/main.5a4ebf7c.chunk.js"
+  },
+  {
+    "revision": "a34979325410ea027a08",
+    "url": "/static/js/runtime~main.d85d922d.js"
+  }
+]);

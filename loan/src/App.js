957eb032cc9f68975291939f22a0a585fb6b540d
MoskKir <47816850+MoskKir@users.noreply.github.com>
2019-09-02 22:50:04 +0300

    Add src files

---
+++ loan/src/App.js
@@ -0,0 +1,74 @@
+import React, {Component, Fragment} from 'react';
+import './skeleton.css';
+import './normalize.css';
+import Form from './components/Form';
+import {calculateTotal} from './helper.js'
+import Result from './components/Result';
+import Message from './components/Message';
+import Spinner from './components/Spinner';
+
+
+class App extends Component {
+
+  state = {
+    total: '',
+    amount: '',
+    term: '',
+    loading: false
+  }
+
+  loanInformation = (amount, term) => {
+    const total = calculateTotal(amount, term);
+
+    this.setState({
+      loading: true
+    }, () => {
+      setTimeout(() => {
+        this.setState({
+          amount,
+          total,
+          term,
+          loading: false
+        })
+      }, 3000);
+    })
+
+  }
+
+  
+
+  render() {
+    const {term, amount, total, loading} = this.state;
+
+    let component;
+
+    if ( total === '' && !loading) {
+      component = <Message />;
+    } else if (loading) {
+      component = <Spinner />
+    } else {
+      component = <Result 
+                    total={total}
+                    amount={amount}
+                    term={term}
+                  />
+    }
+
+    return (
+      <Fragment>
+        <h1>Loan Calculator</h1>
+        <div className="container">
+          <Form 
+            loanInformation={this.loanInformation}
+          />
+          <div className="messages">
+            {component}            
+          </div>            
+        </div>              
+      </Fragment>    
+    );
+  }
+}
+
+export default App;
+
